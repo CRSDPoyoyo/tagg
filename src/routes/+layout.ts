da42cbf4078762e71b8a123a3022f307077e82b6
killbasa <killbasab@gmail.com>
2023-09-22 21:13:12 -0400

    fix: dynamic env vars

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -7,8 +7,8 @@ export const load: LayoutLoad = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
 
 	const supabase = createSupabaseLoadClient({
-		supabaseUrl: env.PUBLIC_SUPABASE_URL,
-		supabaseKey: env.PUBLIC_SUPABASE_ANON_KEY,
+		supabaseUrl: env.PUBLIC_SUPABASE_URL!,
+		supabaseKey: env.PUBLIC_SUPABASE_ANON_KEY!,
 		event: { fetch },
 		serverSession: data.session ?? null
 	});
